7c304ce70f832f63dc0dd0f3dffe257183bc0725
andrefigueira93 <andrefigueira93@gmail.com>
2020-11-25 17:45:32 -0300

    Ajustes de PurgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ['./src/**/*.tsx'],
     options: {
       safelist: [
