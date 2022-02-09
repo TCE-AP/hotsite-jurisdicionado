1148b577210dfb799b0ed1da9baa02597ac65636
Celso Emmanuel <celso.filgueiras@tce.ap.gov.br>
2022-02-09 08:40:59 -0300

    Refatoração do server https

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-var https = require('https');
-var fs = require('fs');
+const https = require('https');
+const fs = require('fs');
 const { parse } = require('url');
 
-var root = '/home/deployer/';
-
-const next = require('next')
+const next = require('next');
 const port = 3000; //parseInt(process.env.PORT, 10) || config.getPort()
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, dir: __dirname })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: __dirname });
+const handle = app.getRequestHandler();
 
-var options = {
-    key: fs.readFileSync(`${root}ssl.key`),
-    cert: fs.readFileSync(`${root}ssl.crt`),
-    // ca: [fs.readFileSync(`${root}root.crt`)],
+const root = '/home/deployer/sslfiles';
+const options = {
+    key: fs.readFileSync(`${root}/tce.ap.gov.br.key`),
+    cert: fs.readFileSync(`${root}/STAR_tce_ap_gov_br.crt`),
+    ca: [fs.readFileSync(`${root}/SectigoRSADomainValidationSecureServerCA.crt`)],
 };
 
 app.prepare().then(() => {
@@ -21,7 +20,7 @@ app.prepare().then(() => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
     }).listen(port, err => {
-        if (err) throw err
-        console.log(`> Ready on localhost:${port}`)
+        if (err) throw err;
+        console.log(`> Ready on localhost:${port}`);
     })
-})
+});
